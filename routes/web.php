c03f5fdacb37c3271c8e0a1d82ea9cae4a8a01bf
Tosho Trajanov <toso.trajanov@gmail.com>
2017-09-30 17:10:30 +0200

    Add show route

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/admin', 'AdminController@index')->name('admin');
-Route::resource('procedure', 'ProcedureController');
+Route::resource('admin/procedure', 'AdminProceduresController');
