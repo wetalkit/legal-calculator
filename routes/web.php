a4dc312ea0e5d94dffd07b55ac50955e4eee35a6
Tosho Trajanov <toso.trajanov@gmail.com>
2017-10-01 15:09:18 +0200

    Implement calculate API endpoint

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/admin', 'AdminController@index')->name('admin');
 Route::resource('admin/procedure', 'AdminProceduresController');
 
-
-Route::get('procedues/calculate', 'ProceduresController@getItemsByProcedure')->name('get_items');
+Route::get('calculate', 'ProceduresController@calculate');
 Route::resource('procedures', 'ProceduresController')->only(['index', 'show']);
