4633252a93c64ae4e43b54c2b0e15ddb8531bf09
Tosho Trajanov <toso.trajanov@gmail.com>
2017-10-03 21:41:24 +0200

    Add more services

--- app/ProcedureFormula.php
+++ app/ProcedureFormula.php
@@ -9,35 +9,83 @@ class ProcedureFormula extends Model
     const FORMULA_LAWYER = 1;
     const FORMULA_NOTAR = 2;
     const FORMULA_KATASTAR = 3;
-    
+    const FORMULA_MVR = 4;
+    const FORMULA_CRM = 5;
+    const FORMULA_ZIS = 6;
+    const FORMULA_UJP = 7;
+    const FORMULA_SUD = 8;
+    const FORMULA_REST = 9;
+
     protected $guarded = ['id'];
     protected $table = 'procedures_formulas';
     public $timestamps = true;
 
+    public static function getCategoryDetails($category)
+    {
+        switch ($category) {
+            case self::FORMULA_LAWYER:
+                return [
+                    'title' => 'lawyer',
+                    'description' => 'Адвокат'
+                ];
+                break;
 
-    public static function getCategoryDetails($category) {
-    	switch ($category) {
-    		case self::FORMULA_LAWYER:
-    			return [
-    				'title' => 'lawyer',
-    				'description' => 'Адвокат'
-    			];
-    			break;
+            case self::FORMULA_NOTAR:
+                return [
+                    'title' => 'notar',
+                    'description' => 'Нотар'
+                ];
+                break;
 
-    		case self::FORMULA_NOTAR:
-    			return [
-    				'title' => 'notar',
-    				'description' => 'Нотар'
-    			];
-    			break;
+            case self::FORMULA_KATASTAR:
+                return [
+                    'title' => 'katastar',
+                    'description' => 'Катастар'
+                ];
+                break;
 
-    		case self::FORMULA_KATASTAR:
-    			return [
-    				'title' => 'katastar',
-    				'description' => 'Катастар'
-    			];
-    			break;
-    	}
+            case self::FORMULA_MVR:
+                return [
+                    'title' => 'mvr',
+                    'description' => 'МВР'
+                ];
+                break;
+
+            case self::FORMULA_CRM:
+                return [
+                    'title' => 'crm',
+                    'description' => 'Централен Регистар'
+                ];
+                break;
+
+            case self::FORMULA_ZIS:
+                return [
+                    'title' => 'zis',
+                    'description' => 'Завод за индустриска сопственост'
+                ];
+                break;
+
+            case self::FORMULA_UJP:
+                return [
+                    'title' => 'ujp',
+                    'description' => 'УЈП'
+                ];
+                break;
+
+            case self::FORMULA_SUD:
+                return [
+                    'title' => 'sud',
+                    'description' => 'Суд'
+                ];
+                break;
+
+            case self::FORMULA_REST:
+                return [
+                    'title' => 'ostanato',
+                    'description' => 'Останато'
+                ];
+                break;
+        }
     }
 
     public static function getFormulas()
@@ -46,6 +94,12 @@ class ProcedureFormula extends Model
             self::FORMULA_LAWYER => 'Адвокат',
             self::FORMULA_NOTAR => 'Нотар',
             self::FORMULA_KATASTAR => 'Катастар',
+            self::FORMULA_MVR => 'МВР',
+            self::FORMULA_CRM => 'Централен Регистар',
+            self::FORMULA_ZIS => 'Завод за индустриска сопственост',
+            self::FORMULA_UJP => 'УЈП',
+            self::FORMULA_SUD => 'Суд',
+            self::FORMULA_REST => 'Останато'
         ];
     }
 }
